e7b69e3d2499609475650f43afda59cac8a089c5
Bodhisattva Duduka <bodhisattva.duduka@gmail.com>
2025-09-09 12:19:25 +0530

    working commit

--- Arrays and ArrayList/src/Swap.java
+++ Arrays and ArrayList/src/Swap.java
@@ -1,12 +1,15 @@
+import java.util.Arrays;
 public class Swap {
 
     public static void main(String[] args) {
         int[] arr = {2, 5, 3, 7, 8, 91, 0};
 
-        Swap(arr, 2, 3);
+        System.out.println(Arrays.toString(arr));
+        swapNumbers(arr, 2, 3);
+        System.out.println(Arrays.toString(arr));
     }
 
-    public static void Swap(int[] arr, int idx1, int idx2) {
+    public static void swapNumbers(int[] arr, int idx1, int idx2) {
         int temp = arr[idx1];
         arr[idx1] = arr[idx2];
         arr[idx2] = temp;
