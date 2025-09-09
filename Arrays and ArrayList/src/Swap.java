0364658fae33174e02ce07d59c7a195edadafa99
Bodhisattva Duduka <bodhisattva.duduka@gmail.com>
2025-09-09 12:06:35 +0530

    basic Swap done, need to import java.util.Arrays and print array

---
+++ Arrays and ArrayList/src/Swap.java
@@ -0,0 +1,14 @@
+public class Swap {
+
+    public static void main(String[] args) {
+        int[] arr = {2, 5, 3, 7, 8, 91, 0};
+
+        Swap(arr, 2, 3);
+    }
+
+    public static void Swap(int[] arr, int idx1, int idx2) {
+        int temp = arr[idx1];
+        arr[idx1] = arr[idx2];
+        arr[idx2] = temp;
+    }
+}
