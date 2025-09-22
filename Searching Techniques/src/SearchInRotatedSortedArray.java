2cd8b68668779216b97c4d5147b6b306d7584f61
Bodhisattva Duduka <bodhisattva.duduka@gmail.com>
2025-09-23 00:22:04 +0530

    search in rotated sorted array

---
+++ Searching Techniques/src/SearchInRotatedSortedArray.java
@@ -0,0 +1,59 @@
+
+public class SearchInRotatedSortedArray {
+
+    public static void main(String[] args) {
+        int[] arr = {4, 5, 6, 7, 0, 1, 2};
+        int target = 0;
+        SearchInRotatedSortedArray obj = new SearchInRotatedSortedArray();
+        System.out.println(obj.search(arr, target));
+    }
+
+    public int search(int[] nums, int target) {
+        int pivot = findPivot(nums);
+        if (pivot == -1) {
+            return binarySearch(nums, target, 0, nums.length - 1);
+        }
+        if (nums[pivot] == target) {
+            return pivot;
+        }
+        if (target >= nums[0]) {
+            return binarySearch(nums, target, 0, pivot - 1);
+        }
+
+        return binarySearch(nums, target, pivot + 1, nums.length - 1);
+    }
+
+    int findPivot(int[] nums) {
+        int low = 0;
+        int high = nums.length - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (mid < high && nums[mid] > nums[mid + 1]) {
+                return mid;
+            }
+            if (mid > low && nums[mid] < nums[mid - 1]) {
+                return mid - 1;
+            }
+            if (nums[mid] <= nums[low]) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+    int binarySearch(int[] nums, int target, int low, int high) {
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (target == nums[mid]) {
+                return mid;
+            } else if (target > nums[mid]) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
