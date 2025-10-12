69da992d27f523bff236c152778b9bb19599816d
Bodhisattva Duduka <bodhisattva.duduka@gmail.com>
2025-10-13 02:26:10 +0530

    Selection Sort

---
+++ Sorting Techniques/src/SelectionSort.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+public class SelectionSort {
+
+    public static void main(String[] args) {
+        int[] arr = {8, 3, 7, 4, 1, 0, 7, 4, 2};
+        selectionSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void selectionSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minIndex = i;
+
+            
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            if (minIndex != i) {
+                int temp = arr[minIndex];
+                arr[minIndex] = arr[i];
+                arr[i] = temp;
+            }
+        }
+    }
+}
