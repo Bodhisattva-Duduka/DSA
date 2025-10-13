ffb149e77e891eadb19bbe63b47f93a52048300d
Bodhisattva Duduka <bodhisattva.duduka@gmail.com>
2025-10-14 01:34:33 +0530

    missing Number 2nd method added

--- Searching Techniques/src/missingNumber.java
+++ Searching Techniques/src/missingNumber.java
@@ -3,7 +3,7 @@ public class missingNumber {
 
     public static void main(String[] args) {
         int[] nums = {9,6,4,2,3,5,7,0,1};
-        System.out.println(findMissingNumber(nums));
+        System.out.println(findingMissingNumber(nums));
     }
 
     public static int findMissingNumber(int[] nums) {
@@ -15,4 +15,36 @@ public class missingNumber {
         return actualSum - sum;
 
     }
+
+
+    //second method
+
+    public static int findingMissingNumber(int[] arr) {
+        int i = 0;
+        while (i < arr.length) {
+            int correct = arr[i];
+            if (arr[i] < arr.length && arr[i] != arr[correct]) {
+                swap(arr, i , correct);
+            } else {
+                i++;
+            }
+        }
+
+        // search for first missing number
+        for (int index = 0; index < arr.length; index++) {
+            if (arr[index] != index) {
+                return index;
+            }
+        }
+
+        // case 2
+        return arr.length;
+    }
+
+    static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
 }
+
---
+++ Sorting Techniques/src/FindAllNumbersDisappearedInAnArray.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class FindAllNumbersDisappearedInAnArray {
+    public static void main(String[] args) {
+        int[] arr = {4,3,2,7,8,2,3,1};
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        
+    }
+}
