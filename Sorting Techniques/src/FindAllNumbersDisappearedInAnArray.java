4bd7be87e21b99ec9688ec691c43f318c62dada1
Bodhisattva Duduka <bodhisattva.duduka@gmail.com>
2025-10-14 23:10:49 +0530

    betterment version

--- Sorting Techniques/src/FindAllNumbersDisappearedInAnArray.java
+++ Sorting Techniques/src/FindAllNumbersDisappearedInAnArray.java
@@ -15,12 +15,9 @@ public class FindAllNumbersDisappearedInAnArray {
         int i = 0;
         while (i < nums.length) {
             int correct = nums[i] - 1;
-            if (nums[i] != correct) {
+            if (nums[i] != nums[correct]) {
                 swap(nums, i, correct);
             } else {
-                i++;
-            }
-            if (nums[i] == nums[correct]) {
                 i++;
             }
         }
