46e044a2cfbc921767e4d058f429176446d515d6
Bodhisattva Duduka <bodhisattva.duduka@gmail.com>
2025-11-14 00:51:06 +0530

    Find single number

--- Bitwise Operators/src/FindSingleNumber.java
+++ Bitwise Operators/src/FindSingleNumber.java
@@ -1,3 +1,13 @@
 public class FindSingleNumber {
-    
+    public static void main(String[] args) {
+        int[] arr = {2,3,7,3,1,9,4,2,1,9,7};
+        System.out.println(findNumber(arr));
+    }
+    static int findNumber(int[] arr){
+        int result = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            result = result ^ arr[i];
+        }
+        return result;
+    }
 }
