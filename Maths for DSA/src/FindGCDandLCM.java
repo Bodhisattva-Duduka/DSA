f02beb70d265d26846b01231b368390c6f23ac46
Bodhisattva Duduka <bodhisattva.duduka@gmail.com>
2025-11-19 02:07:30 +0530

    added LCM

---
+++ Maths for DSA/src/FindGCDandLCM.java
@@ -0,0 +1,20 @@
+
+public class FindGCDandLCM {
+
+    public static void main(String[] args) {
+        int n = 40, m = 60;
+        FindGCDandLCM obj = new FindGCDandLCM();
+        System.out.println("GCD: " + obj.gcd(n, m));
+        System.out.println("LCM: " + obj.lcm(n, m));
+    }
+
+    static int gcd(int a, int b) {
+        if (a == 0) {
+            return b;
+        }
+        return gcd(b % a, a);
+    }
+    static int lcm(int a, int b){
+        return a * b / gcd(a,b);
+    }
+}
