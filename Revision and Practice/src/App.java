611bc263a6c22078c6c50392787801382bd72d08
Bodhisattva Duduka <bodhisattva.duduka@gmail.com>
2025-12-27 18:18:38 +0530

    revised leetcode 1295

--- Revision and Practice/src/App.java
+++ Revision and Practice/src/App.java
@@ -1,5 +1,22 @@
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+    public static void main(String[] args) {
+        int[] arr = {12, 345, 2, 6, 7896};
+        System.out.println(findNumberWithEvenNoOfDigits(arr));
     }
+
+    // leetcode 1295
+
+    public static int findNumberWithEvenNoOfDigits(int[] arr) {
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            int noOfDigits = (int) Math.log(arr[i]) + 1;
+            if (noOfDigits % 2 == 0) {
+                count++;
+            }
+        }
+        return count;
+    }
+
 }
