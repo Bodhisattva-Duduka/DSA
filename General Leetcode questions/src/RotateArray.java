30d14369bd35a0bedd3f8d6c032cfb4df92cff06
Bodhisattva Duduka <bodhisattva.duduka@gmail.com>
2025-12-31 22:27:11 +0530

    index formula

--- General Leetcode questions/src/RotateArray.java
+++ General Leetcode questions/src/RotateArray.java
@@ -1,28 +1,19 @@
 import java.util.*;
 public class RotateArray {
     public static void main(String[] args) {
-        int[] nums = {-1,1};
-        int k = 5;
+        int[] nums = {1,2,3,4,5,6,7};
+        int k = 3;
         RotateArray obj = new RotateArray();
         obj.rotate(nums, k);
         System.out.println(Arrays.toString(nums));
     }
     public void rotate(int[] nums, int k) {
         int[] arr = new int[nums.length];
+        int n = arr.length;
         int index = 0;
-        int secondIndex = 0;
         for(int i = 0; i<nums.length; i++){
-            index = i+k;
-            if(index<nums.length){
-                arr[index] = nums[i];
-            }
-            else {
-                secondIndex = k - (arr.length - i);
-                while(secondIndex>=arr.length){
-                    secondIndex = secondIndex - arr.length;
-                }
-                arr[secondIndex] = nums[i];
-            }
+            index = (k + i) % n;
+            arr[index] = nums[i];
         }
         for (int i = 0; i < arr.length; i++) {
             nums[i] = arr[i];
