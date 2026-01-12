b1c20014f9d97104abddf8dcad606af1f1fd274f
Bodhisattva Duduka <bodhisattva.duduka@gmail.com>
2026-01-12 18:20:23 +0530

    PrefixMax and PrefixMin

--- General Leetcode questions/src/PrefixSum.java
+++ General Leetcode questions/src/PrefixSum.java
@@ -4,7 +4,8 @@ public class PrefixSum {
     public static void main(String[] args) {
         int[] arr = {2,4,1,3,5};
         PrefixSum obj = new PrefixSum();
-        System.out.println(Arrays.toString(obj.prefixSumArray(arr)));
+        // System.out.println(Arrays.toString(obj.prefixSumArray(arr)));
+        obj.prefixMaxAndPrefixMin(arr);
     }
     long[] prefixSumArray(int[] arr){
         long[] P = new long[arr.length + 1];
@@ -13,5 +14,17 @@ public class PrefixSum {
             P[i + 1] = P[i] + arr[i];
         }
         return P;
+    }
+    void prefixMaxAndPrefixMin(int[] arr){
+        int[] prefixMax = new int[arr.length];
+        int[] prefixMin = new int[arr.length];
+        prefixMax[0] = arr[0];
+        prefixMin[0] = arr[0];
+        for(int i = 1; i<arr.length; i++){
+            prefixMax[i] = Math.max(prefixMax[i-1], arr[i]);
+            prefixMin[i] = Math.min(prefixMin[i - 1], arr[i]);
+        }
+        System.out.println(Arrays.toString(prefixMax));
+        System.out.println(Arrays.toString(prefixMin));
     }
 }
