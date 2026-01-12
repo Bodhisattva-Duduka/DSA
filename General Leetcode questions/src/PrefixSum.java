64cb498eceb977f135bcd7813128728670c7e2b5
Bodhisattva Duduka <bodhisattva.duduka@gmail.com>
2026-01-12 18:40:57 +0530

    Added More

--- General Leetcode questions/src/PrefixSum.java
+++ General Leetcode questions/src/PrefixSum.java
@@ -5,7 +5,9 @@ public class PrefixSum {
         int[] arr = {2,4,1,3,5};
         PrefixSum obj = new PrefixSum();
         // System.out.println(Arrays.toString(obj.prefixSumArray(arr)));
-        obj.prefixMaxAndPrefixMin(arr);
+        // obj.prefixMaxAndPrefixMin(arr);
+        // System.out.println(Arrays.toString(obj.suffixSumArray(arr)));
+        obj.suffixMaxAndsuffixMin(arr);
     }
     long[] prefixSumArray(int[] arr){
         long[] P = new long[arr.length + 1];
@@ -27,4 +29,27 @@ public class PrefixSum {
         System.out.println(Arrays.toString(prefixMax));
         System.out.println(Arrays.toString(prefixMin));
     }
+    long[] suffixSumArray(int[] arr){
+        int n = arr.length;
+        long[] S = new long[n + 1];
+        S[n] = 0;
+        for(int i = n - 1; i>=0; i--){
+            S[i] = S[i+1] + arr[i];
+        }
+        return S;
+    }
+    void suffixMaxAndsuffixMin(int[] arr){
+        int n = arr.length;
+        int[] suffixMax = new int[n];
+        int[] suffixMin = new int[n];
+        suffixMax[n-1] = arr[n-1];
+        suffixMin[n-1] = arr[n-1];
+        for(int i = n - 2; i>=0 ; i--){
+            suffixMax[i] = Math.max(suffixMax[i+1], arr[i]);
+            suffixMin[i] = Math.min(suffixMin[i+1], arr[i]);
+        }
+        System.out.println(Arrays.toString(suffixMax));
+        System.out.println(Arrays.toString(suffixMin));
+    }
+
 }
