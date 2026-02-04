2aa35ca92371facb95082846839b1f43c2771bce
Bodhisattva Duduka <bodhisattva.duduka@gmail.com>
2026-02-05 01:50:34 +0530

    Practice on Classes and objects

---
+++ OOP/src/ClassesAndObjects.java
@@ -0,0 +1,16 @@
+public class ClassesAndObjects {
+    public static void main(String[] args) {
+        Student student1 = new Student();
+        student1.name = "Bodhisattva";
+        student1.rollno = 64;
+        student1.marks = 95;
+        System.out.println(student1.name);
+        System.out.println(student1.rollno);
+        System.out.println(student1.marks);
+    }
+}
+class Student {
+    String name;
+    int rollno;
+    int marks;
+}
