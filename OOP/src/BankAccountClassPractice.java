93b86166daba8ed38fb9c1d20eac68bf14b88273
Bodhisattva Duduka <bodhisattva.duduka@gmail.com>
2026-02-10 12:18:36 +0530

    Task 1

--- OOP/src/BankAccountClassPractice.java
+++ OOP/src/BankAccountClassPractice.java
@@ -1,15 +1,24 @@
 public class BankAccountClassPractice{
     public static void main(String[] args) {
-        BankAccount bodhisattva = new BankAccount();
-        bodhisattva.accountNumber = 564;
-        bodhisattva.deposit(5000);
-        bodhisattva.withdraw(3000);
-        System.out.println(bodhisattva.getBalance());
-        BankAccount rahul = new BankAccount();
-        rahul.accountNumber = 6666;
-        rahul.deposit(4000);
-        rahul.withdraw(1000);
-        System.out.println(rahul.getBalance());
+        // BankAccount bodhisattva = new BankAccount();
+        // bodhisattva.accountNumber = 564;
+        // bodhisattva.deposit(5000);
+        // bodhisattva.withdraw(3000);
+        // System.out.println(bodhisattva.getBalance());
+        // BankAccount rahul = new BankAccount();
+        // rahul.accountNumber = 6666;
+        // rahul.deposit(4000);
+        // rahul.withdraw(1000);
+        // System.out.println(rahul.getBalance());
+
+
+        // Task 1:
+        BankAccount bodhisattva = new BankAccount(564, 2000);
+        BankAccount rahul = new BankAccount(666, 1500);
+        System.out.println(bodhisattva.balance);
+        System.out.println(rahul.balance);
+
+        
     }
 }
 class BankAccount {
@@ -27,5 +36,24 @@ class BankAccount {
     double getBalance(){
         return balance;
     }
+
+    BankAccount(int accountNumber, double balance){
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
 }
+
+// Task 1: Parameterized Constructor
+
+// Modify your BankAccount class.
+
+// Requirements
+
+// Account must have:
+//       accountNumber
+//       initialBalance
+// These must be set at object creation
+// No account should exist without them
+
+// ----------------------------------------------------
 
