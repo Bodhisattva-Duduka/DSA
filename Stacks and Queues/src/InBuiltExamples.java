f7f493e1b23aa18a6cd40595c51ef8674e9a1bd8
Bodhisattva Duduka <bodhisattva.duduka@gmail.com>
2026-04-05 23:20:10 +0530

    Inbuilt Examples

---
+++ Stacks and Queues/src/InBuiltExamples.java
@@ -0,0 +1,45 @@
+import java.util.*;
+public class InBuiltExamples {
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+
+        stack.push(10);
+        stack.push(29);
+        stack.push(54);
+        stack.push(23);
+        stack.push(42);
+
+        // System.out.println(stack.pop());
+        // System.out.println(stack.pop());
+
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(10);
+        queue.add(53);
+        queue.add(23);
+        queue.add(29);
+        queue.add(40);
+
+        // System.out.println(queue.remove());
+        // System.out.println(queue.remove());
+
+        // Stack like
+        Deque<Integer> deque1 = new ArrayDeque<>();
+        deque1.push(12);
+        deque1.push(55);
+        deque1.push(32);
+
+        // System.out.println(deque1.pop());
+
+        // Queue like
+        Deque<Integer> deque2 = new ArrayDeque<>();
+        deque2.offerLast(23);
+        deque2.offerLast(38);
+        deque2.offerFirst(93);
+        deque2.offerFirst(83);
+
+        // System.out.println(deque2.peekFirst());
+        // System.out.println(deque2.peekLast());
+        // System.out.println(deque2.pollLast());
+        // System.out.println(deque2.pollLast());
+    }
+}
