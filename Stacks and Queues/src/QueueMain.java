cac37fd950aee10ccad7ec5d731b4155c084417d
Bodhisattva Duduka <bodhisattva.duduka@gmail.com>
2026-04-07 01:18:41 +0530

    Circular Queue

--- Stacks and Queues/src/QueueMain.java
+++ Stacks and Queues/src/QueueMain.java
@@ -2,14 +2,32 @@
 
 public class QueueMain {
     public static void main(String[] args) throws Exception{
-        CustomQueue queue = new CustomQueue(4);
+        // CustomQueue queue = new CustomQueue(4);
+
+        // queue.insert(23);
+        // queue.insert(39);
+        // queue.insert(20);
+        // queue.insert(37);
+
+        // queue.display();
+
+        // queue.remove();
+        // queue.remove();
+
+        // System.out.println(queue.remove());
+        // queue.display();
+
+        // queue.insert(48);
+        // System.out.println(queue.front());
+
+        CircularQueue queue = new CircularQueue();
 
         queue.insert(23);
         queue.insert(39);
         queue.insert(20);
         queue.insert(37);
 
-        // queue.display();
+        queue.display();
 
         queue.remove();
         queue.remove();
