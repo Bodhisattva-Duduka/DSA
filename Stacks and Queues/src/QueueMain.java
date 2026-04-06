5a09b0edcac7912e105d4d3f178005dc7e9140a0
Bodhisattva Duduka <bodhisattva.duduka@gmail.com>
2026-04-06 23:52:04 +0530

    CustomQueue

---
+++ Stacks and Queues/src/QueueMain.java
@@ -0,0 +1,23 @@
+
+
+public class QueueMain {
+    public static void main(String[] args) throws Exception{
+        CustomQueue queue = new CustomQueue(4);
+
+        queue.insert(23);
+        queue.insert(39);
+        queue.insert(20);
+        queue.insert(37);
+
+        // queue.display();
+
+        queue.remove();
+        queue.remove();
+
+        System.out.println(queue.remove());
+        queue.display();
+
+        queue.insert(48);
+        System.out.println(queue.front());
+    }
+}
