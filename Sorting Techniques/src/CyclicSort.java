0b5c01450ae9ab85227fbe13884eee936d65f0c2
Bodhisattva Duduka <bodhisattva.duduka@gmail.com>
2026-07-21 19:35:32 +0530

    first missing positive

--- Sorting Techniques/src/CyclicSort.java
+++ Sorting Techniques/src/CyclicSort.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 
 public class CyclicSort {
     public static void main(String[] args) {
-        int[] arr = {3,4,-1,1};
+        int[] arr = {4,5,6,7,8};
         // int[] arr = {5,4,1,2,3};
         cyclic(arr);
         System.out.println(Arrays.toString(arr));
@@ -13,12 +13,22 @@ public class CyclicSort {
         while(i<arr.length){
 
             int correctIndex = arr[i] - 1;
-            if(arr[correctIndex] != arr[i]){
+
+            if(arr[i] > 0 && arr[i] <= arr.length && arr[i]>0 && arr[correctIndex] != arr[i]){
                 int temp = arr[correctIndex];
                 arr[correctIndex] = arr[i];
                 arr[i] = temp;
             } else {
                 i++;
+            }
+        }
+        int f = 1;
+        for(int j = 0; j<arr.length; j++){
+            if(arr[j]>0 && arr[j] == f){
+                f++;
+            } else {
+                System.out.println(f);
+                break;
             }
         }
     }
